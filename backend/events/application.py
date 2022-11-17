f51d40307412f91a07e906eb69dfb78808e96b9c
Mario Juárez Castillo <mariojc29@gmail.com>
2022-11-17 10:51:57 -0600

    test

--- backend/events/application.py
+++ backend/events/application.py
@@ -1,19 +1,15 @@
 """Application module."""
 
 from fastapi import FastAPI
-
 from .containers import Container
-
 from .View import endpointsEvent
+
 
 def create_app() -> FastAPI:
     container = Container()
 
-
-
     app = FastAPI()
     app.container = container
-
 
     app.include_router(endpointsEvent.router)
     return app
